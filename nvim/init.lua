59355fbfeacf1d0d954febdc67c3ecf442b84e9a
marberri <saubuny@gmail.com>
2026-06-22 03:19:11 -0400

    dunstrc

--- nvim/init.lua
+++ nvim/init.lua
@@ -49,7 +49,7 @@ vim.pack.add({
 	"https://github.com/nvim-lualine/lualine.nvim",
 	"https://github.com/ibhagwan/fzf-lua",
 	"https://github.com/romus204/tree-sitter-manager.nvim",
-	"https://github.com/rebelot/kanagawa.nvim",
+	{ src = "https://codeberg.org/evergarden/nvim.git", name = "evergarden" },
 })
 
 local function packadd(name)
@@ -138,13 +138,14 @@ require("tree-sitter-manager").setup({
 vim.api.nvim_set_hl(0, "Error", { link = "Normal" }) 
 
 -- Colorscheme
--- I'm not the biggest fan of this theme, or any theme I can find, but this is 
--- the best at making things easier to read while also not standing out too much.
-packadd("kanagawa.nvim")
-require("kanagawa").setup({
-    keywordStyle = { italic = false },
-    statementStyle = { bold = false },
-})
+packadd("evergarden")
 vim.o.background = "dark"
-vim.cmd.colorscheme "kanagawa-dragon"
+require("evergarden").setup({
+  theme = {
+    variant = 'winter', -- 'winter'|'fall'|'spring'|'summer'
+    accent = 'green',
+  }, 
+})
+vim.cmd.colorscheme "evergarden"
+
 
